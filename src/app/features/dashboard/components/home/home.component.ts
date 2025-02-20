a91334b0398ceb04823df795f3a8510500773954
ddsal07 <danidiaz0799@gmail.com>
2025-02-19 21:48:23 -0500

    improve desing of de home dashboard

--- src/app/features/dashboard/components/home/home.component.ts
+++ src/app/features/dashboard/components/home/home.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
-import { SidebarComponent } from 'src/app/shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,4 +9,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent { }
+export class SidebarComponent {
+  isOpen = true;
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
+}
