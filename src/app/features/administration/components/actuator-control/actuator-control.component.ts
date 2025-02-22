dd12ac550c0b37297aa7c549d5866aa04e2e59c2
ddsal07 <danidiaz0799@gmail.com>
2025-02-21 21:05:03 -0500

    Se mejora la administración

--- src/app/features/administration/components/actuator-control/actuator-control.component.ts
+++ src/app/features/administration/components/actuator-control/actuator-control.component.ts
@@ -1,18 +1,50 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActuatorService } from '../../services/actuator.service';
+import { DashboardService } from '../../../dashboard/services/dashboard.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-actuator-control',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './actuator-control.component.html',
   styleUrls: ['./actuator-control.component.scss']
 })
-export class ActuatorControlComponent {
+export class ActuatorControlComponent implements OnInit, OnDestroy {
   private actuatorService = inject(ActuatorService);
+  private dashboardService = inject(DashboardService);
 
   lucesEncendidas = signal(false);
+  ventiladoresEncendidos: boolean = false;
+  humidificadorEncendido: boolean = false;
+  desiredTemperature!: number;
+  desiredHumidity!: number;
+  latestTemperature: number | undefined;
+  latestHumidity: number | undefined;
+  intervalId: any;
+
+  ngOnInit(): void {
+    this.fetchSensorData();
+    this.intervalId = setInterval(() => {
+      this.fetchSensorData();
+    }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  fetchSensorData() {
+    this.dashboardService.getSensorData(1, 10, false).subscribe(data => {
+      data.sort((a: any, b: any) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+
+      this.latestTemperature = data[0].temperature;
+      this.latestHumidity = data[0].humidity;
+    });
+  }
 
   toggleLuces() {
     this.lucesEncendidas.update(value => !value);
@@ -24,4 +56,21 @@ export class ActuatorControlComponent {
     });
   }
 
+  toggleVentiladores() {
+    this.ventiladoresEncendidos = !this.ventiladoresEncendidos;
+  }
+
+  toggleHumidificador() {
+    this.humidificadorEncendido = !this.humidificadorEncendido;
+  }
+
+  setTemperature() {
+    console.log(`Temperatura establecida a: ${this.desiredTemperature}°C`);
+    // Lógica para establecer la temperatura
+  }
+
+  setHumidity() {
+    console.log(`Humedad establecida a: ${this.desiredHumidity}%`);
+    // Lógica para establecer la humedad
+  }
 }
